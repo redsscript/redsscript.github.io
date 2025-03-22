81b1352df9cbb6401adce3b5fa76c4bb38973fe3
redsscript <reds.script@gmail.com>
2025-03-22 13:42:28 +0700

    hero icon animasi

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -1,15 +1,27 @@
 var heroIcon = document.querySelector("#hero-icon")
 
-//  gsap.to(heroIcon, {
+ gsap.to(heroIcon, {
+    scrollTrigger:
+    {
+        trigger: heroIcon,
+        start: ' 30% top', 
+        end: '20% top', 
+        scrub: 4, 
+        pin:true,
+      
+    },
+    opacity:0,
+});
+
+// gsap.fromTo('#action-button',{ x : -300},{
 //     scrollTrigger:
 //     {
-//         trigger: heroIcon,
+//         trigger: '#action-button',
 //         start: ' 30% top', 
 //         end: '20% top', 
 //         scrub: 4, 
 //         pin:true,
 //         markers: true,
 //     },
-     
-   
+//     x:0,
 // });
